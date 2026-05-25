2655594364504ad87a5a00aedda7b3df424d90cc
qla-dev <kulasinn@gmail.com>
2026-05-26 00:24:15 +0200

    polz

--- src/data/userMahalas.ts
+++ src/data/userMahalas.ts
@@ -2,40 +2,530 @@ export const USER_MAHALAS = [
   {
     id: "user-dobrinja",
     name: "Dobrinja",
-    center: { latitude: 43.83027762393895, longitude: 18.347061916058255 },
+    center: {
+      latitude: 43.83027762393895,
+      longitude: 18.347061916058255,
+    },
     coordinates: [
-      { latitude: 43.835820107069715, longitude: 18.343581323792847 },
-      { latitude: 43.835597308936784, longitude: 18.34235648716493 },
-      { latitude: 43.83546670274794, longitude: 18.341376617862515 },
-      { latitude: 43.83542060637777, longitude: 18.340386097806817 },
-      { latitude: 43.83505183413435, longitude: 18.338841738580214 },
-      { latitude: 43.83354599048866, longitude: 18.337606251198913 },
-      { latitude: 43.83253951258082, longitude: 18.337105665794425 },
-      { latitude: 43.828805408074224, longitude: 18.334432326719448 },
-      { latitude: 43.82220484538918, longitude: 18.3514628812252 },
-      { latitude: 43.82437181605449, longitude: 18.35384864996154 },
-      { latitude: 43.82353423742803, longitude: 18.355904245345815 },
-      { latitude: 43.82114438427178, longitude: 18.359291184891138 },
-      { latitude: 43.8271918338426, longitude: 18.36661890315248 },
-      { latitude: 43.829816294378304, longitude: 18.356969882140508 },
-      { latitude: 43.83365347730955, longitude: 18.346146485237032 },
+      {
+        latitude: 43.835820107069715,
+        longitude: 18.343581323792847,
+      },
+      {
+        latitude: 43.835597308936784,
+        longitude: 18.34235648716493,
+      },
+      {
+        latitude: 43.83546670274794,
+        longitude: 18.341376617862515,
+      },
+      {
+        latitude: 43.83542060637777,
+        longitude: 18.340386097806817,
+      },
+      {
+        latitude: 43.83505183413435,
+        longitude: 18.338841738580214,
+      },
+      {
+        latitude: 43.83354599048866,
+        longitude: 18.337606251198913,
+      },
+      {
+        latitude: 43.83253951258082,
+        longitude: 18.337105665794425,
+      },
+      {
+        latitude: 43.828805408074224,
+        longitude: 18.334432326719448,
+      },
+      {
+        latitude: 43.82220484538918,
+        longitude: 18.3514628812252,
+      },
+      {
+        latitude: 43.82437181605449,
+        longitude: 18.35384864996154,
+      },
+      {
+        latitude: 43.82353423742803,
+        longitude: 18.355904245345815,
+      },
+      {
+        latitude: 43.82114438427178,
+        longitude: 18.359291184891138,
+      },
+      {
+        latitude: 43.8271918338426,
+        longitude: 18.36661890315248,
+      },
+      {
+        latitude: 43.829816294378304,
+        longitude: 18.356969882140508,
+      },
+      {
+        latitude: 43.83365347730955,
+        longitude: 18.346146485237032,
+      },
     ],
     holes: [],
   },
   {
     id: "user-bascarsija",
     name: "Baščaršija",
-    center: { latitude: 43.8592315004674, longitude: 18.4277220897024 },
+    center: {
+      latitude: 43.8592315004674,
+      longitude: 18.4277220897024,
+    },
     coordinates: [
-      { latitude: 43.85959297647673, longitude: 18.420549776161884 },
-      { latitude: 43.86022615967557, longitude: 18.424990949231006 },
-      { latitude: 43.86076591709314, longitude: 18.425609978216322 },
-      { latitude: 43.86147693623302, longitude: 18.430886120614485 },
-      { latitude: 43.85998910227687, longitude: 18.431555516718603 },
-      { latitude: 43.85908703094089, longitude: 18.434019761898018 },
-      { latitude: 43.85857518495364, longitude: 18.434648186801947 },
-      { latitude: 43.856844405613984, longitude: 18.426878949667465 },
-      { latitude: 43.85652579094244, longitude: 18.42035956801164 },
+      {
+        latitude: 43.85959297647673,
+        longitude: 18.420549776161884,
+      },
+      {
+        latitude: 43.86022615967557,
+        longitude: 18.424990949231006,
+      },
+      {
+        latitude: 43.86076591709314,
+        longitude: 18.425609978216322,
+      },
+      {
+        latitude: 43.86147693623302,
+        longitude: 18.430886120614485,
+      },
+      {
+        latitude: 43.85998910227687,
+        longitude: 18.431555516718603,
+      },
+      {
+        latitude: 43.85908703094089,
+        longitude: 18.434019761898018,
+      },
+      {
+        latitude: 43.85857518495364,
+        longitude: 18.434648186801947,
+      },
+      {
+        latitude: 43.856844405613984,
+        longitude: 18.426878949667465,
+      },
+      {
+        latitude: 43.85652579094244,
+        longitude: 18.42035956801164,
+      },
+    ],
+    holes: [],
+  },
+  {
+    id: "user-c5",
+    name: "C5",
+    center: {
+      latitude: 43.826596864460925,
+      longitude: 18.344607184445277,
+    },
+    coordinates: [
+      {
+        latitude: 43.828839675109,
+        longitude: 18.34260674707754,
+      },
+      {
+        latitude: 43.827885522640074,
+        longitude: 18.341484514591198,
+      },
+      {
+        latitude: 43.827454662143346,
+        longitude: 18.3410410589629,
+      },
+      {
+        latitude: 43.826714194024454,
+        longitude: 18.341219871716188,
+      },
+      {
+        latitude: 43.82611540689007,
+        longitude: 18.341828058062454,
+      },
+      {
+        latitude: 43.82435593618341,
+        longitude: 18.346896662904914,
+      },
+      {
+        latitude: 43.82650265777828,
+        longitude: 18.348490867910588,
+      },
+    ],
+    holes: [],
+  },
+  {
+    id: "user-aerodromsko-naselje",
+    name: "Aerodromsko naselje",
+    center: {
+      latitude: 43.82889607023262,
+      longitude: 18.338530740627924,
+    },
+    coordinates: [
+      {
+        latitude: 43.83157722570358,
+        longitude: 18.33658738484757,
+      },
+      {
+        latitude: 43.82945996434351,
+        longitude: 18.335169366250476,
+      },
+      {
+        latitude: 43.82815745445112,
+        longitude: 18.33541669507554,
+      },
+      {
+        latitude: 43.826423991697965,
+        longitude: 18.340293298273426,
+      },
+      {
+        latitude: 43.8279047776828,
+        longitude: 18.341697217114582,
+      },
+      {
+        latitude: 43.82879579570253,
+        longitude: 18.342488032158048,
+      },
+      {
+        latitude: 43.828853487058275,
+        longitude: 18.342656857841394,
+      },
+    ],
+    holes: [],
+  },
+  {
+    id: "user-dobrinja-2",
+    name: "Dobrinja 2",
+    center: {
+      latitude: 43.82913152262724,
+      longitude: 18.34637338560263,
+    },
+    coordinates: [
+      {
+        latitude: 43.829008122781055,
+        longitude: 18.342490033407632,
+      },
+      {
+        latitude: 43.83162108030137,
+        longitude: 18.344516572794817,
+      },
+      {
+        latitude: 43.82929149146864,
+        longitude: 18.350245568888212,
+      },
+      {
+        latitude: 43.82662280180127,
+        longitude: 18.348212554186343,
+      },
+    ],
+    holes: [],
+  },
+  {
+    id: "user-dobrinja-1",
+    name: "Dobrinja 1",
+    center: {
+      latitude: 43.82430777340277,
+      longitude: 18.35002526420891,
+    },
+    coordinates: [
+      {
+        latitude: 43.82415128490447,
+        longitude: 18.347050475660673,
+      },
+      {
+        latitude: 43.826319268561456,
+        longitude: 18.348582735519066,
+      },
+      {
+        latitude: 43.82644447973396,
+        longitude: 18.34870174599351,
+      },
+      {
+        latitude: 43.824573676350816,
+        longitude: 18.35312300603561,
+      },
+      {
+        latitude: 43.82221972302712,
+        longitude: 18.351485959955493,
+      },
+      {
+        latitude: 43.82391523721898,
+        longitude: 18.34699121322679,
+      },
+    ],
+    holes: [],
+  },
+  {
+    id: "user-dobrinja-5",
+    name: "Dobrinja 5",
+    center: {
+      latitude: 43.83311400888608,
+      longitude: 18.34310569606723,
+    },
+    coordinates: [
+      {
+        latitude: 43.83021677487056,
+        longitude: 18.343260838347135,
+      },
+      {
+        latitude: 43.83134520385785,
+        longitude: 18.341659786205074,
+      },
+      {
+        latitude: 43.83186156110285,
+        longitude: 18.34090204491486,
+      },
+      {
+        latitude: 43.83235139161109,
+        longitude: 18.340772343322993,
+      },
+      {
+        latitude: 43.832784663069255,
+        longitude: 18.339898602735094,
+      },
+      {
+        latitude: 43.8340110816182,
+        longitude: 18.341757554342774,
+      },
+      {
+        latitude: 43.83397508277565,
+        longitude: 18.341549028798394,
+      },
+      {
+        latitude: 43.835597506855144,
+        longitude: 18.343152953100816,
+      },
+      {
+        latitude: 43.83584352632714,
+        longitude: 18.343429491773573,
+      },
+      {
+        latitude: 43.835830228003246,
+        longitude: 18.344028658898086,
+      },
+      {
+        latitude: 43.83467923440207,
+        longitude: 18.34498297488844,
+      },
+      {
+        latitude: 43.833754110098035,
+        longitude: 18.34579255046657,
+      },
+    ],
+    holes: [],
+  },
+  {
+    id: "user-dobrinja-3",
+    name: "Dobrinja 3",
+    center: {
+      latitude: 43.8314268032543,
+      longitude: 18.348275925945273,
+    },
+    coordinates: [
+      {
+        latitude: 43.831642775640645,
+        longitude: 18.344464654859234,
+      },
+      {
+        latitude: 43.83236302834991,
+        longitude: 18.34490644525281,
+      },
+      {
+        latitude: 43.833076898564094,
+        longitude: 18.3456928321535,
+      },
+      {
+        latitude: 43.83345295176079,
+        longitude: 18.34624065224161,
+      },
+      {
+        latitude: 43.83150254843857,
+        longitude: 18.351966255742973,
+      },
+      {
+        latitude: 43.8291951262543,
+        longitude: 18.350393481941694,
+      },
+    ],
+    holes: [],
+  },
+  {
+    id: "user-dobrinja-4",
+    name: "Dobrinja 4",
+    center: {
+      latitude: 43.827436564976004,
+      longitude: 18.355210333394687,
+    },
+    coordinates: [
+      {
+        latitude: 43.826634011467625,
+        longitude: 18.3487701605773,
+      },
+      {
+        latitude: 43.83158448547208,
+        longitude: 18.352440116046285,
+      },
+      {
+        latitude: 43.829861531766255,
+        longitude: 18.357216883482227,
+      },
+      {
+        latitude: 43.82855037269309,
+        longitude: 18.361515974174573,
+      },
+      {
+        latitude: 43.82581029343845,
+        longitude: 18.360257703728053,
+      },
+      {
+        latitude: 43.824734399710245,
+        longitude: 18.359115939804305,
+      },
+      {
+        latitude: 43.824448612179225,
+        longitude: 18.357857669357784,
+      },
+      {
+        latitude: 43.82333066545823,
+        longitude: 18.356972219784282,
+      },
+    ],
+    holes: [],
+  },
+  {
+    id: "user-ferhadija",
+    name: "Ferhadija",
+    center: {
+      latitude: 43.85853949272103,
+      longitude: 18.42589866148801,
+    },
+    coordinates: [
+      {
+        latitude: 43.85877571142259,
+        longitude: 18.42458641802762,
+      },
+      {
+        latitude: 43.85796652494357,
+        longitude: 18.424864576651633,
+      },
+      {
+        latitude: 43.85824831174401,
+        longitude: 18.42712852166111,
+      },
+      {
+        latitude: 43.85916742277394,
+        longitude: 18.427015129611675,
+      },
+    ],
+    holes: [],
+  },
+  {
+    id: "user-test-a",
+    name: "Test A",
+    center: {
+      latitude: 43.8312719568462,
+      longitude: 18.33651203173377,
+    },
+    coordinates: [
+      {
+        latitude: 43.831227869048796,
+        longitude: 18.336542221419116,
+      },
+      {
+        latitude: 43.83125664926367,
+        longitude: 18.336449309482887,
+      },
+      {
+        latitude: 43.831317686452,
+        longitude: 18.336472872164144,
+      },
+      {
+        latitude: 43.831285622620335,
+        longitude: 18.33658372386893,
+      },
+    ],
+    holes: [],
+  },
+  {
+    id: "user-test-b",
+    name: "Test B",
+    center: {
+      latitude: 43.83137973270527,
+      longitude: 18.336579228621414,
+    },
+    coordinates: [
+      {
+        latitude: 43.83133481141472,
+        longitude: 18.336606094294687,
+      },
+      {
+        latitude: 43.83137341278244,
+        longitude: 18.336495227981942,
+      },
+      {
+        latitude: 43.83141729942498,
+        longitude: 18.336564528891873,
+      },
+      {
+        latitude: 43.831393407198945,
+        longitude: 18.336651063317152,
+      },
+    ],
+    holes: [],
+  },
+  {
+    id: "user-test-c",
+    name: "Test C",
+    center: {
+      latitude: 43.82602210056012,
+      longitude: 18.345298022366816,
+    },
+    coordinates: [
+      {
+        latitude: 43.826009428304296,
+        longitude: 18.345264899022027,
+      },
+      {
+        latitude: 43.82599688843504,
+        longitude: 18.34530099884029,
+      },
+      {
+        latitude: 43.82603141834676,
+        longitude: 18.34533102993173,
+      },
+      {
+        latitude: 43.82605066715437,
+        longitude: 18.345295161673217,
+      },
+    ],
+    holes: [],
+  },
+  {
+    id: "user-test-d",
+    name: "Test D",
+    center: {
+      latitude: 43.82604220966854,
+      longitude: 18.345255946186462,
+    },
+    coordinates: [
+      {
+        latitude: 43.82601502405862,
+        longitude: 18.34524849769988,
+      },
+      {
+        latitude: 43.82605325878231,
+        longitude: 18.345290304806426,
+      },
+      {
+        latitude: 43.826067366064784,
+        longitude: 18.345260464138818,
+      },
+      {
+        latitude: 43.82603318976847,
+        longitude: 18.34522451810073,
+      },
     ],
     holes: [],
   },
